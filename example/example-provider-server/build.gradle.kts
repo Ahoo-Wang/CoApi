253beb25df0cb844c494a3d693f5cb87619f2f28
Ahoo Wang <ahoowang@qq.com>
2025-06-03 18:01:29 +0800

    test: add JUnit platform launcher and engine to example servers
    - Add junit-platform-launcher and junit-jupiter-engine as testRuntimeOnly dependencies
    - Update both example-consumer-server and example-provider-server build.gradle.kts files

--- example/example-provider-server/build.gradle.kts
+++ example/example-provider-server/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.withType<Test> {
